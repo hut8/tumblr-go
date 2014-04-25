2fca6829504d53c7858cc63e9a1c09b48cc312cc
Liam Bowen <LiamBowen@gmail.com>
2014-04-25 14:59:00 -0400

    Constants for post types specified by API, function for determining correct post URL, skeleton of Posts liked by a blog as well as posts posted in a blog

--- tumblr.go
+++ tumblr.go
@@ -16,3 +16,37 @@ type Post struct{}
 const (
 	urlBase = "http://api.tumblr.com/v2/"
 )
+
+// Post Types
+const (
+	Text   = "text"
+	Quote  = "quote"
+	Link   = "link"
+	Answer = "answer"
+	Video  = "video"
+	Audio  = "audio"
+	Photo  = "photo"
+	Chat   = "chat"
+)
+
+// Where the request is directed to
+func entityURL(entityType string) (*url.URL, error) {
+	url, err := url.Parse(urlBase)
+	if err != nil {
+		return nil, err
+	}
+	url.Path = path.Join(url.Path, entityType)
+	return url, nil
+}
+
+// Posts posted by a blog
+func (blog Blog) Posts(params PostRequestParams) []Post {
+	// TODO
+	return nil
+}
+
+// Posts liked by a blog
+func (blog Blog) Likes() []Post {
+	// TODO
+	return nil
+}
