74c85645ac10c4d005fbc97702c788ce282ba47e
Liam Bowen <LiamBowen@gmail.com>
2014-04-25 16:35:15 -0400

    Factor out blog likes into its own file

---
+++ blog_likes.go
@@ -0,0 +1,17 @@
+package tumblr
+
+// Posts liked by a blog
+func (blog Blog) Likes(params LimitOffset) (*BlogLikes, error) {
+	url, err := blog.blogEntityURL("likes")
+	if err != nil {
+		return err
+	}
+	addLimitOffset(url, params)
+
+	return nil, nil
+}
+
+type BlogLikes struct {
+	Likes      []Post
+	TotalCount int64
+}
