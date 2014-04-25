3d340819fd5385519cdf56f1a8bd411fb0903520
Liam Bowen <LiamBowen@gmail.com>
2014-04-25 15:36:09 -0400

    Take advantage of zero initialization (don't require pointers for nil values).  Process remaining query string values.

--- tumblr.go
+++ tumblr.go
@@ -48,22 +48,22 @@ func (blog Blog) entityURL(entityType string) (*url.URL, error) {
 
 // Request Parameter Types
 type PostRequestParams struct {
-	PostType   *string
-	Id         *int64
-	Tag        *string
+	PostType   string
+	Id         int64
+	Tag        string
 	Limit      int
 	Offset     int
 	ReblogInfo bool
 	NotesInfo  bool
-	Filter     *string
+	Filter     string
 }
 
 func (params PostRequestParams) validatePostRequestParams() error {
-	if params.Limit < 1 || params.Limit > 20 {
+	if params.Limit < 0 || params.Limit > 20 {
 		return TumblrError{"Post request parameter limit out of range"}
 	}
-	if params.Filter != nil &&
-		!(*params.Filter == "html" || *params.Filter == "raw") {
+	if params.Filter != "" &&
+		!(params.Filter == "html" || params.Filter == "raw") {
 		return TumblrError{`Filter, if specified, must be either "html" or "raw"`}
 	}
 	return nil
@@ -76,14 +76,38 @@ func (blog Blog) Posts(params PostRequestParams) []Post {
 	if err != nil {
 		return nil
 	}
-	if params.PostType != nil {
-		url.Path = path.Join(url.Path, *params.PostType)
+
+	// PostType
+	if params.PostType != "" {
+		url.Path = path.Join(url.Path, params.PostType)
 	}
 
-	// Query string parameters
+	// Id
+	if params.Id != 0 {
+		url.Query().Set("id", string(params.Id))
+	}
+
+	// Tag
+	if params.Tag != "" {
+		url.Query().Set("tag", params.Tag)
+	}
+
+	// Limit
+	if params.Limit != 0 {
+		url.Query().Set("limit", string(params.Limit))
+	}
+
+	// Offset
+	if params.Offset != 0 {
+		url.Query().Set("offset", string(params.Offset))
+	}
+
+	// ReblogInfo
 	if params.ReblogInfo {
 		url.Query().Set("reblog_info", "true")
 	}
+
+	// NotesInfo
 	if params.NotesInfo {
 		url.Query().Set("notes_info", "true")
 	}
