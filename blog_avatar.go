80e30ca8715cbe18ea2327bf3aa5443b62891be5
Liam Bowen <LiamBowen@gmail.com>
2014-04-25 16:36:06 -0400

    Factor out avatar stuff into its own file

---
+++ blog_avatar.go
@@ -0,0 +1,33 @@
+package tumblr
+
+import (
+	"net/url"
+	"path"
+)
+
+func validateAvatarSize(size int32) bool {
+	switch size {
+	case 0, 16, 24, 30, 40, 48, 64, 96, 128, 512:
+		return true
+	default:
+		return false
+	}
+}
+
+func (blog Blog) Avatar(size int32) (*url.URL, error) {
+	url, err := blog.blogEntityURL("avatar")
+	if err != nil {
+		return nil, err
+	}
+
+	if size != 0 {
+		if !validateAvatarSize(size) {
+			return nil, TumblrError{"Invalid Avatar size specified"}
+		}
+		url.Path = path.Join(url.Path, string(size))
+	}
+
+	// TODO send to API
+	// TODO parse results in "avatar_url"
+	return nil, nil
+}
