7777c11c8a10c6dde89c8beaf0223d43d647e285
Liam Bowen <LiamBowen@gmail.com>
2014-05-02 00:07:02 -0400

    Factor out the bloggy stuff into its own file

---
+++ blog.go
@@ -0,0 +1,25 @@
+package tumblr
+
+import (
+	"net/url"
+	"path"
+)
+
+type Blog struct {
+	BaseHostname string
+	t Tumblr
+}
+
+// Where the request is directed to
+func (blog Blog) blogEntityURL(entityType string) (*url.URL, error) {
+	url, err := blog.t.apiURL()
+	if err != nil {
+		return nil, err
+	}
+	url.Path = path.Join(
+		url.Path,
+		"blog",
+		blog.BaseHostname,
+		entityType)
+	return url, nil
+}
