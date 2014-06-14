7dca7aa846d55299a9f7f658c89f92ec028ee7e6
Liam Bowen <LiamBowen@gmail.com>
2014-06-14 03:54:52 -0400

    Posts request for a blog goes in its own file

---
+++ blog_posts.go
@@ -0,0 +1,66 @@
+package tumblr
+
+type PostRequestParams struct {
+	PostType   string
+	Id         int64
+	Tag        string
+	ReblogInfo bool
+	NotesInfo  bool
+	Filter     string
+	LimitOffset
+}
+
+func (params PostRequestParams) validatePostRequestParams() error {
+	if params.Limit < 0 || params.Limit > 20 {
+		return TumblrError{"Post request parameter limit out of range"}
+	}
+	if params.Filter != "" &&
+		!(params.Filter == "html" || params.Filter == "raw") {
+		return TumblrError{`Filter, if specified, must be either "html" or "raw"`}
+	}
+	return nil
+}
+
+// Posts posted by a blog
+func (blog *Blog) Posts(params PostRequestParams) []Post, error {
+	// Build URL
+	url, err := blog.blogEntityURL("posts")
+	if err != nil {
+		return nil, err
+	}
+
+	// PostType
+	if params.PostType != "" {
+		url.Path = path.Join(url.Path, params.PostType)
+	}
+
+	// Id
+	if params.Id != 0 {
+		url.Query().Set("id", string(params.Id))
+	}
+
+	// Tag
+	if params.Tag != "" {
+		url.Query().Set("tag", params.Tag)
+	}
+
+	// ReblogInfo
+	if params.ReblogInfo {
+		url.Query().Set("reblog_info", "true")
+	}
+
+	// NotesInfo
+	if params.NotesInfo {
+		url.Query().Set("notes_info", "true")
+	}
+
+	addLimitOffset(url, params.LimitOffset)
+
+	data, err := callAPI(url)
+
+	var posts []Post
+	// TODO Deserialize results
+
+
+	return posts, nil
+}
