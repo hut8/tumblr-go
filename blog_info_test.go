070eebec07bb29eb7f6321ef8ae326349b961497
Liam Bowen <LiamBowen@gmail.com>
2014-06-15 19:32:25 -0400

    Working test for blog info

--- blog_info_test.go
+++ blog_info_test.go
@@ -1,7 +1,27 @@
 package tumblr
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestInfo(t *testing.T) {
-	// TODO
+	c := makeTumblr()
+	b := c.NewBlog("lacecard.tumblr.com")
+	i, err := b.Info()
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	if i.Updated < 1 {
+		t.Errorf("Blog marked as never updated (updated=%d)", i.Updated)
+		return
+	}
+	if i.Likes < 1 {
+		t.Errorf("Blog marked as having no likes (likes=%d)", i.Updated)
+		return
+	}
+	if i.Name != "lacecard" {
+		t.Errorf("Blog information gives wrong name (name=%s)", i.Name)
+		return
+	}
 }
